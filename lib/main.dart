00d9ac1bbdc0c73ba452d4bda085a1f648092bb3
fkhrlysn <fakhrulyusaini@gmail.com>
2023-07-20 12:57:09 +0800

    Add stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,35 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
   //all widget start with capital letter
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('The Great App'),
-      centerTitle: true,
-      backgroundColor: Colors.teal[300],
-    ),
-  body: Center(
-    child: Text('Welcome!',
-      style: TextStyle(
-        fontSize: 20.0,
-        decorationThickness: 20.0,
-        fontWeight: FontWeight.bold,
-        color: Colors.grey[600],
-        fontFamily: 'RobotoCondensed',
-      ),
-    ),
-  ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          backgroundColor: Colors.teal[300],
-          onPressed: () {
-            // Add the onPressed callback to specify what happens when the button is clicked
-            // For now, you can leave it empty if you don't want any action to be performed.
-          },
-    ),
-    )
+  home: Home(),
   ));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+    title: Text('The Great App'),
+    centerTitle: true,
+    backgroundColor: Colors.teal[300],
+    ),
+    body: Center(
+    child: Text('Welcome!',
+    style: TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.grey[600],
+    fontFamily: 'RobotoCondensed',
+    ),
+    ),
+    ),
+      floatingActionButton: FloatingActionButton(
+      child:Text('click'),
+      backgroundColor: Colors.teal[300],
+      onPressed: () {
+    },
+    ),
+    );
+  }
+}
